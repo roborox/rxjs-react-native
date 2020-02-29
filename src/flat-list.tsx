907abc73108e103aad4b3b342d4bd5bf5b017581
Roborox dev <dev@roborox.org>
2020-02-29 08:17:16 +0300

    fix(*): RxFlatList supports refreshing

--- src/flat-list.tsx
+++ src/flat-list.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react"
-import { FlatList, FlatListProps, ListRenderItemInfo, RefreshControl, RefreshControlProps } from "react-native"
+import { FlatList, FlatListProps, ListRenderItemInfo } from "react-native"
 import { Observable, of } from "rxjs"
 import { useRx } from "@roborox/rxjs-react/build"
 import { filter } from "rxjs/operators"
@@ -20,12 +20,11 @@ interface Props<T> extends Omit<FlatListProps<T>, "data" | "renderItem" | "refre
 	data: Observable<List<T> | T[] | undefined>
 	renderItem: RxListRenderItem<T>
 	refreshing?: Observable<boolean>
-	onRefresh?: () => void;
 }
 
-export function RxFlatList<T>({ data, renderItem, refreshing, onRefresh, ...rest }: Props<T>) {
+export function RxFlatList<T>({ data, renderItem, refreshing: ref, ...rest }: Props<T>) {
 	const list = useRx(ifDefined(data)) || []
-	const refreshingPlain = useRx(refreshing || of(false), false)
+	const refreshing = useRx(ref || of(false), false)
 	const newRenderItem = useCallback((item: ListRenderItemInfo<T>) => {
 		return renderItem({...item, first: item.index === 0, last: (item.index + 1 === list?.length)})
 	}, [list])
@@ -36,9 +35,5 @@ export function RxFlatList<T>({ data, renderItem, refreshing, onRefresh, ...rest
 	} else {
 		array = list.toJSON()
 	}
-	let refreshControl: React.ReactElement<RefreshControlProps> | undefined
-	if (refreshing) {
-		refreshControl = <RefreshControl refreshing={refreshingPlain} onRefresh={onRefresh}/>
-	}
-	return <FlatList data={array} renderItem={newRenderItem} refreshControl={refreshControl} {...rest} />
+	return <FlatList data={array} renderItem={newRenderItem} refreshing={refreshing} {...rest} />
 }
